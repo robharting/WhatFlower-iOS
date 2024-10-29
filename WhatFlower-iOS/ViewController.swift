9e48bbdcdbd5f5529c82ebce01ce4654721ebdc1
Rob Harting <robharting@hotmail.com>
2024-10-29 08:19:52 +0100

    create flower recgniser via MLModel FlowerClassifier

--- WhatFlower-iOS/ViewController.swift
+++ WhatFlower-iOS/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage  = info[.originalImage] as? UIImage {
+            
+            guard let convertedCiImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert image to CIImage.")
+            }
+            
+            detect(flowerImage: convertedCiImage)
+            
+            imageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(flowerImage: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Cannot import model.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let classificationResult = request.results?.first as? VNClassificationObservation else {
+                fatalError("Can't load model")
+            }
+                
+            self.navigationItem.title = classificationResult.identifier.capitalized
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
     }
 
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
